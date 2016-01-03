cb98ee5d610d6007221f8da30066736b3f7cbd32
Avinash Raj <avistylein3105@gmail.com>
2016-01-03 15:40:00 +0530

    Implementing the regex engine 1st stage

--- static/js/controller.js
+++ static/js/controller.js
@@ -5,11 +5,24 @@
 var app = angular.module('regex_tester', []);
 
 app.controller('MainCtrl', function ($scope) {
-    var _data = '';
+    $scope.input_regex = '';
+    $scope.input_data = '';
+    $scope.modifier = '';
     $scope.do_parsing = {
+        data: function () {
+            if ($scope.input_regex != '' && $scope.input_data != '') {
+                //return $scope.input_regex + ' | ' + $scope.input_data;
+                var re = $scope.input_regex;
+                var data = $scope.input_data;
+                var mod = $scope.modifier;
+                var match = new RegExp(re, mod).exec(data);
+                var dict = {};
+                while (match != null || match != undefined) {
+                    dict[match.index] = match[0]
+                }
 
-        data: function(newData) {
-            return arguments.length ? (_data = newData) : _data;
+            }
+
         }
     };
 });
